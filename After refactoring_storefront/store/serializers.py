a717e41df9b141a330f8435ebf512244a2ce3b60
Naga pavan <nagapavan@gmail.com>
2023-06-19 17:56:01 +0530

    before model serializering and re-structuring

--- After refactoring_storefront/store/serializers.py
+++ After refactoring_storefront/store/serializers.py
@@ -1,11 +1,28 @@
 from rest_framework import serializers
 from decimal import Decimal
+from .models import Product, Collection
 
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    #1st method
+    # collection = serializers.PrimaryKeyRelatedField(
+    #     queryset= Collection.objects.all()
+    # )
+    #2 method
+    # collection = serializers.StringRelatedField()
+    #3 method
+    #collection = CollectionSerializer()
+    collection = serializers.HyperlinkedRelatedField(
+        queryset=Collection.objects.all(),
+        view_name='collection_detail'
+    )
 
-    def calculate_tax(self, Product):
-        return Product.unit_price * Decimal(1.1)
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
--- After refactoring_storefront/store/views.py
+++ After refactoring_storefront/store/views.py
@@ -7,8 +7,8 @@ from .serializers import ProductSerializer
 # Create your views here.
 @api_view()
 def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
+    queryset = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(queryset, many=True, context={'request':request})
     return Response(serializer.data)
 
 @api_view()
@@ -16,3 +16,7 @@ def product_detail(request, id):
     product = get_object_or_404(Product, pk= id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+
+@api_view()
+def collection_detail(request, pk):
+    return Response('ok')
