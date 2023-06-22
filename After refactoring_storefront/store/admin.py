78ec2fbc97b86323664e16cbc033bbc496a72af4
Naga pavan <nagapavan@gmail.com>
2023-06-23 01:14:36 +0530

    Authentication System in Admin page, Djoser,JSON web token setup.

--- After refactoring_storefront/store/admin.py
+++ After refactoring_storefront/store/admin.py
@@ -46,9 +46,11 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name','last_name','membership']
+    autocomplete_fields = ['user']
     list_editable = ['membership']
     list_per_page = 10
-    ordering = ['first_name','last_name']
+    list_select_related = ['user']
+    ordering = ['user__first_name','user__last_name']
     search_fields = ['first_name__istartswith','last_name__istartswith']
 
 class OrderItemInline(admin.TabularInline):
--- After refactoring_storefront/storefront/settings.py
+++ After refactoring_storefront/storefront/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
+    'djoser',
     'playground',
     'debug_toolbar',
     'store',
@@ -145,4 +146,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING':False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
+
+AUTH_USER_MODEL = 'store_custom.User'
